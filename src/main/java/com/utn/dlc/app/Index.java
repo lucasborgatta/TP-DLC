948027964cec5e1fcf56d95d44895032ea586f9e
Maner <nahuam00@gmail.com>
2022-05-17 12:28:28 -0300

    asd
    asd

--- src/main/java/com/utn/dlc/app/Index.java
+++ src/main/java/com/utn/dlc/app/Index.java
@@ -1,14 +1,15 @@
 package com.utn.dlc.app;
 
+import org.apache.coyote.Request;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
@@ -24,9 +25,7 @@ public class Index {
     public void index() throws IOException {
         File folder = new File("D:\\opt\\Facu\\DLC\\TP\\src\\main\\resources\\Prueba");
         int contador = 0;
-        boolean flagPalabra = false;
         ArrayList<String> stopWords = stopWords();
-        stopWords = stopWords();
         for (File file : folder.listFiles()) {
             this.sendPostDocumento(contador, file.getName());
             String linea;
@@ -37,13 +36,11 @@ public class Index {
             while (st.hasMoreTokens()) {
                 palabra = st.nextToken();
                 if (!stopWords.contains(palabra)) {
-                    sendPostPalabras(palabra, flagPalabra);
-                    flagPalabra = true;
+                    sendPostPalabras(palabra, false);
                 }
 
             }
             contador++;
-            flagPalabra = false;
         }
     }
 
@@ -57,6 +54,7 @@ public class Index {
             words.add(line);
         }
         return words;
+//        System.out.println(words);
     }
 
 
@@ -110,56 +108,34 @@ public class Index {
         }
     }
 
-    public void sendPostPalabras(String nombrePalabra, boolean flag) throws IOException {
+    public static void sendPostPalabras(String nombrePalabra, boolean flag) throws IOException {
         HttpPost post = new HttpPost("http://localhost:8080/palabras/add");
         List<BasicNameValuePair> urlParameters = new ArrayList<>();
-        ArrayList<String> getPalabra = new ArrayList<String>();
+        urlParameters.add(new BasicNameValuePair("nombre", nombrePalabra));
+        urlParameters.add(new BasicNameValuePair("flag", Boolean.toString(flag)));
+        post.setEntity(new UrlEncodedFormEntity(urlParameters));
 
-        if (Objects.nonNull(getPalabraById(nombrePalabra))) {
-            getPalabra = getPalabraById(nombrePalabra);
-            if (flag == false) {
-                int contadorPalabra = Integer.parseInt(getPalabra.get(1));
-                sendPutPalabra(nombrePalabra, contadorPalabra+1);
-            }
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+             CloseableHttpResponse response = httpClient.execute(post)) {
 
-        } else {
-            urlParameters.add(new BasicNameValuePair("nombre", nombrePalabra));
-            urlParameters.add(new BasicNameValuePair("cant_Documentos", "1"));
-            post.setEntity(new UrlEncodedFormEntity(urlParameters));
-
-            try (CloseableHttpClient httpClient = HttpClients.createDefault();
-                 CloseableHttpResponse response = httpClient.execute(post)) {
-                System.out.println(EntityUtils.toString(response.getEntity()));
-            }
+            System.out.println(EntityUtils.toString(response.getEntity()));
         }
     }
 
-    public ArrayList<String> getPalabraById(String value) {
+    public void getPalabraById() throws IOException {
         HttpClient client = HttpClientBuilder.create().build();
-        String url = "http://localhost:8080/palabras/id?nombre=" + value;
-        HttpGet get = new HttpGet(url);
-        ArrayList<String> getResponse = new ArrayList<String>();
-        //List<BasicNameValuePair> urlParameters = new ArrayList<>();
-        //urlParameters.add(new BasicNameValuePair("nombre", "bancamos"));
-        //request.setHeader("nombre", "bancamos");
+        HttpGet request = new HttpGet("http://localhost:8080/palabras/id");
+        request.addHeader("nombre", "bancamos");
 
         try {
-            HttpResponse response = client.execute(get);
+            HttpResponse response = client.execute(request);
             HttpEntity entity = response.getEntity();
             if (entity != null) {
                 try (InputStream stream = entity.getContent()) {
                     BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
                     String line;
-                    String palabra;
                     while ((line = reader.readLine()) != null) {
-                        StringTokenizer st = new StringTokenizer(line, " \n:,\"{}");
-                        while (st.hasMoreTokens()) {
-                            palabra = st.nextToken();
-                            if (!(palabra.equals("nombre") || palabra.equals("cantDocumentos"))) {
-                                System.out.println(palabra);
-                                getResponse.add(palabra);
-                            }
-                        }
+                        System.out.println(line);
                     }
                 }
             }
@@ -184,21 +160,6 @@ public class Index {
 //                System.out.println(reader.toString());
 //            }
 //        }
-        return getResponse;
     }
 
-    public void sendPutPalabra(String nombrePalabra, int cant_documentos) throws IOException {
-        HttpPut put = new HttpPut("http://localhost:8080/palabras/put");
-        List<BasicNameValuePair> urlParameters = new ArrayList<>();
-
-        urlParameters.add(new BasicNameValuePair("nombre", nombrePalabra));
-        urlParameters.add(new BasicNameValuePair("cant_documentos", String.valueOf(cant_documentos)));
-        put.setEntity(new UrlEncodedFormEntity(urlParameters));
-
-        try (CloseableHttpClient httpClient = HttpClients.createDefault();
-             CloseableHttpResponse response = httpClient.execute(put)) {
-            System.out.println(EntityUtils.toString(response.getEntity()));
-        }
-    }
 }
-
