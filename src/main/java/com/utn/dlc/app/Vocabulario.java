846611db0a25ec88d6ef6eb465fb85130ed6428a
Maner <nahuam00@gmail.com>
2022-05-19 18:35:17 -0300

    Vocabulario
    asd

--- src/main/java/com/utn/dlc/app/Vocabulario.java
+++ src/main/java/com/utn/dlc/app/Vocabulario.java
@@ -6,17 +6,16 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 
-import javax.persistence.criteria.CriteriaBuilder;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
-public class CalculoPeso {
+public class Vocabulario {
 
-    public void calculoPeso(){
+    public void setVocabulario(){
         File folder = new File("C:\\Users\\nahue\\OneDrive\\Documentos\\GitHub\\TP-DLC\\src\\main\\resources\\Prueba");
-        HashMap<Integer, String> palabras = getAllPalabras();
+        HashMap<String, ArrayList<Integer>> vocabulario = getAllPalabras();
 
         for (File file : folder.listFiles()) {
 
@@ -88,12 +87,13 @@ public class CalculoPeso {
         return getResponse;
     }
 
-    public HashMap<Integer, String> getAllPalabras() {
+    public HashMap<String, ArrayList<Integer>> getAllPalabras() {
         HttpClient client = HttpClientBuilder.create().build();
         String url = "http://localhost:8080/palabras/all";
         HttpGet get = new HttpGet(url);
-        HashMap<Integer, String> getResponse = new HashMap<>();
-        int key = 0;
+        HashMap<String, ArrayList<Integer>> getResponse = new HashMap<>();
+        ArrayList<ArrayList<Integer>> array = new ArrayList<>();
+        ArrayList<String> palabras = new ArrayList<>();
 
         try {
             HttpResponse response = client.execute(get);
@@ -104,17 +104,24 @@ public class CalculoPeso {
                     String line;
                     String palabra;
                     while ((line = reader.readLine()) != null) {
-                        StringTokenizer st = new StringTokenizer(line, " \n1234567890:,\"{}");
+                        StringTokenizer st = new StringTokenizer(line, " \n:,\"{}");
                         while (st.hasMoreTokens()) {
                             palabra = st.nextToken();
                             if (!(palabra.equals("nombre") || palabra.equals("cantDocumentos"))) {
-                                System.out.println(palabra);
-                                getResponse.put(key, palabra);
-                                key++;
+                                if (palabra.contains("0123456789")){
+                                    ArrayList<Integer> cantDoc = new ArrayList<>();
+                                    cantDoc.add(Integer.parseInt(palabra));
+                                    array.add(cantDoc);
+                                }else{
+                                    palabras.add(palabra);
+                                }
                             }
                         }
                     }
                 }
+            }
+            for (int i = 0; i < palabras.size(); i++) {
+                getResponse.put(palabras.get(i), array.get(i));
             }
         } catch (IOException e) {
             e.printStackTrace();
