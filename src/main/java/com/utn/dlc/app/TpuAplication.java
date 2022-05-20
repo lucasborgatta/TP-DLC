9e926a29a0103494ecd273610b273fe33051fd53
Maner <nahuam00@gmail.com>
2022-05-20 16:38:45 -0300

    asd
    asd

--- src/main/java/com/utn/dlc/app/TpuAplication.java
+++ src/main/java/com/utn/dlc/app/TpuAplication.java
@@ -6,15 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class TpuAplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TpuAplication.class, args);
-
-		Vocabulario cp = new Vocabulario();
-		cp.setVocabulario();
 	}
 
 }
