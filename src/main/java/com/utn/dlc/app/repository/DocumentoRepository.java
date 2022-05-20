04c13dd32832dbfa50f5ea951b1cd3b85961f057
Maner <nahuam00@gmail.com>
2022-05-20 16:38:45 -0300

    asd
    asd

--- src/main/java/com/utn/dlc/app/repository/DocumentoRepository.java
+++ src/main/java/com/utn/dlc/app/repository/DocumentoRepository.java
@@ -9,14 +9,10 @@ import org.springframework.stereotype.Repository;
 import java.util.Collection;
 import java.util.List;
 
-@Repository
+@Repository("docRepo")
 public interface DocumentoRepository extends JpaRepository<Documento, Long> {
 
-//    @Query(value = "SELECT D.nombre FROM Documento D WHERE D.id LIKE :filtro")
-//    List<Documento> searchAll(@Param("filtro") Long filtro );
-
-//    @Query(value = "SELECT * FROM documentos d where d.id = 1", nativeQuery = true);
-//    public  List<Documento> findAllDocuments();
-
+    @Query(value = "SELECT d.* from documentos d where  d.id=?1", nativeQuery = true)
+    public List<Documento> getDocumentoById(Long id);
 
 }
