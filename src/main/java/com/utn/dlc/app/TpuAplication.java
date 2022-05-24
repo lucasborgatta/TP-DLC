c855a491b04373158565c6e1511690a84642c406
Maner <nahuam00@gmail.com>
2022-05-24 17:43:53 -0300

    asd
    asd

--- src/main/java/com/utn/dlc/app/TpuAplication.java
+++ src/main/java/com/utn/dlc/app/TpuAplication.java
@@ -3,11 +3,15 @@ package com.utn.dlc.app;
 
 import com.utn.dlc.app.controller.DocumentoController;
 import com.utn.dlc.app.entity.Documento;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.print.Doc;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +19,7 @@ import java.util.List;
 @SpringBootApplication
 @EnableJpaAuditing
 public class TpuAplication {
-
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TpuAplication.class, args);
-		Index index = new Index();
-		index.getPalabraById("bancamos");
 	}
-
 }
