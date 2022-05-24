c2398ba8ffcd4ecd726ef51a92c6fbb8e1a53102
Maner <nahuam00@gmail.com>
2022-05-24 17:43:53 -0300

    asd
    asd

--- src/main/java/com/utn/dlc/app/repository/PalabraRepository.java
+++ src/main/java/com/utn/dlc/app/repository/PalabraRepository.java
@@ -1,7 +1,13 @@
 package com.utn.dlc.app.repository;
 
+import com.utn.dlc.app.entity.Documento;
 import com.utn.dlc.app.entity.Palabra;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PalabraRepository extends JpaRepository<Palabra, String> {
+    @Query(value = "INSERT INTO palabras (nombre, cant_documentos) VALUES (?1, ?2)", nativeQuery = true)
+    public List<Documento> insertPalabra(String nombre, int cant_documentos);
 }
