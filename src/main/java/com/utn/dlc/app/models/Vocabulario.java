7658706dbe6dd89160e484484d7a3928e86dbce8
Maner <nahuam00@gmail.com>
2022-05-27 19:10:28 -0300

    bascara
    bascara

--- src/main/java/com/utn/dlc/app/models/Vocabulario.java
+++ src/main/java/com/utn/dlc/app/models/Vocabulario.java
@@ -1,132 +1,38 @@
 package com.utn.dlc.app.models;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-
 import java.io.*;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.List;
 
 public class Vocabulario {
 
-    public void setVocabulario(){
-        File folder = new File("C:\\Users\\nahue\\OneDrive\\Documentos\\GitHub\\TP-DLC\\src\\main\\resources\\Prueba");
-        HashMap<String, ArrayList<Integer>> vocabulario = getAllPalabras();
-
-        for (File file : folder.listFiles()) {
-
-
-        }
+    public void setVocabulario() throws Exception {
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://tpi-dlc.mysql.database.azure.com:3306/tpi?useSSL=false&serverTimezone=America/Argentina/Buenos_Aires";
+        Class.forName(driver).newInstance();
+        Connection connection = DriverManager.getConnection(url, "Programa", "Programa123456789");
+        HashMap<String, ArrayList<Integer>> vocabulario;
+        vocabulario = getDatosVocabulario(connection);
     }
 
-    public ArrayList<String> getPalabraById(String value) {
-        HttpClient client = HttpClientBuilder.create().build();
-        String url = "http://localhost:8080/palabras/id?nombre=" + value;
-        HttpGet get = new HttpGet(url);
-        ArrayList<String> getResponse = new ArrayList<String>();
-
-        try {
-            HttpResponse response = client.execute(get);
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                try (InputStream stream = entity.getContent()) {
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-                    String line;
-                    String palabra;
-                    while ((line = reader.readLine()) != null) {
-                        StringTokenizer st = new StringTokenizer(line, " \n:,\"{}");
-                        while (st.hasMoreTokens()) {
-                            palabra = st.nextToken();
-                            if (!(palabra.equals("nombre") || palabra.equals("cantDocumentos"))) {
-                                System.out.println(palabra);
-                                getResponse.add(palabra);
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    public HashMap<String, ArrayList<Integer>> getDatosVocabulario(Connection connection) throws SQLException {
+        HashMap<String, ArrayList<Integer>> hash = new HashMap<>();
+        String consulta = "select distinct p.nombre_palabra, count(p.id_documento), max(p.frecuencia) from posteos p group by  p.nombre_palabra";
+        Statement statement = connection.createStatement();
+        ResultSet rs;
+        rs = statement.executeQuery(consulta);
+        while (rs.next()){
+            ArrayList<Integer> arrayList= new ArrayList<>();
+            int frecuenciaMax = rs.getInt(3);
+            int cantidadDocs = rs.getInt(2);
+            String palabra = rs.getString(1);
+            arrayList.add(cantidadDocs);
+            arrayList.add(frecuenciaMax);
+            hash.put(palabra, arrayList);
         }
-        return getResponse;
-    }
-
-    public ArrayList<String> getPosteoById(Long id_documento, String nombre_palabra) {
-        HttpClient client = HttpClientBuilder.create().build();
-        String url = "http://localhost:8080/posteos/id?id_documento=" + id_documento + "&nombre_palabra=" + nombre_palabra;
-        HttpGet get = new HttpGet(url);
-        ArrayList<String> getResponse = new ArrayList<String>();
-
-        try {
-            HttpResponse response = client.execute(get);
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                try (InputStream stream = entity.getContent()) {
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-                    String line;
-                    String palabra;
-                    while ((line = reader.readLine()) != null) {
-                        StringTokenizer st = new StringTokenizer(line, " \n:,\"{}");
-                        while (st.hasMoreTokens()) {
-                            palabra = st.nextToken();
-                            if (!(palabra.equals("id_documento") || palabra.equals("nombre_palabra") || palabra.equals("frecuencia") || palabra.equals("peso"))) {
-                                System.out.println(palabra);
-                                getResponse.add(palabra);
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return getResponse;
-    }
-
-    public HashMap<String, ArrayList<Integer>> getAllPalabras() {
-        HttpClient client = HttpClientBuilder.create().build();
-        String url = "http://localhost:8080/palabras/all";
-        HttpGet get = new HttpGet(url);
-        HashMap<String, ArrayList<Integer>> getResponse = new HashMap<>();
-        ArrayList<ArrayList<Integer>> array = new ArrayList<>();
-        ArrayList<String> palabras = new ArrayList<>();
-
-        try {
-            HttpResponse response = client.execute(get);
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                try (InputStream stream = entity.getContent()) {
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-                    String line;
-                    String palabra;
-                    while ((line = reader.readLine()) != null) {
-                        StringTokenizer st = new StringTokenizer(line, " \n:,\"{}");
-                        while (st.hasMoreTokens()) {
-                            palabra = st.nextToken();
-                            if (!(palabra.equals("nombre") || palabra.equals("cantDocumentos"))) {
-                                if (palabra.contains("0123456789")){
-                                    ArrayList<Integer> cantDoc = new ArrayList<>();
-                                    cantDoc.add(Integer.parseInt(palabra));
-                                    array.add(cantDoc);
-                                }else{
-                                    palabras.add(palabra);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            for (int i = 0; i < palabras.size(); i++) {
-                getResponse.put(palabras.get(i), array.get(i));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return getResponse;
+        return hash;
     }
 
 }
