96a9f38b1c3222064cf375839eb4c7340c85ac1e
Guille Gribaudo <80915763+GuilleTG20@users.noreply.github.com>
2022-06-11 18:12:29 -0300

    bhuy

--- src/main/java/com/utn/dlc/app/TpuAplication.java
+++ src/main/java/com/utn/dlc/app/TpuAplication.java
@@ -24,29 +24,29 @@ public class TpuAplication {
 
 		// Esto es para poder hacer la conexion a la base y tener una conexion que podamos manejar con codigo
 		String driver = "com.mysql.cj.jdbc.Driver";
-		String url = "root@127.0.0.1:3306";
+		String url = "jdbc:mysql://127.0.0.1:3306/sys?useSSL=false&serverTimezone=America/Argentina/Buenos_Aires";
 		//String url = "jdbc:mysql://tpi-dlc.mysql.database.azure.com:3306/tpi?useSSL=false&serverTimezone=America/Argentina/Buenos_Aires";
 
 
 		Class.forName(driver).newInstance();
-		Connection connection = DriverManager.getConnection(url, "root", null);
+		Connection connection = DriverManager.getConnection(url, "root", "1234");
 		//Connection connection = DriverManager.getConnection(url, "Programa", "Programa123456789");
 
 		// INDEXACION
-		/*
+
 		Index in = new Index();
 		in.index(connection);
 
-		 */
 
 
-		long endtime = System.currentTimeMillis() - startTime;
-		System.out.println(endtime);
 
-		System.out.println("Buscando");
-		Buscador buscador = new Buscador("quijote", connection);
-		buscador.buscarPalabra(10);
-		System.out.println("Terminado");
+//		long endtime = System.currentTimeMillis() - startTime;
+//		System.out.println(endtime);
+//
+//		System.out.println("Buscando");
+//		Buscador buscador = new Buscador("quijote", connection);
+//		buscador.buscarPalabra(10);
+//		System.out.println("Terminado");
 
 		/*
 		Scanner scanner = new Scanner(System.in);
